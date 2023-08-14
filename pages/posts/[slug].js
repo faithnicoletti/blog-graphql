1b9ac2ba4d7cc8347417510bb641c0bc390b38ce
faithnicoletti <faithalicia22@gmail.com>
2023-08-14 17:41:27 -0400

    trying to get mobile render to work

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,6 @@
 import {GraphQLClient, gql} from 'graphql-request';
 import Format from '../../layout/format';
 import Link from 'next/link';
-// import ReactHtmlParser from 'react-html-parser';
 
 const graphcms = new GraphQLClient(
   "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/cll6qjzv804qm01um9wvpgn63/master"
@@ -73,26 +72,18 @@ export default function BlogPost({ post }) {
         <br />
       </div>
       <div className="px-5 py-8 md:px-20">
-        {/* Display the image on top */}
         <div className="mb-4">
           <img src={post.coverPhoto.url} alt="" className="mx-auto" width={800} height={800} />
         </div>
         <div className="text-center">
-          {/* Display the title */}
           <div className="text-3xl md:text-5xl font-bold text-gray-800 mb-2">{post.title}</div>
           <br />
-          {/* Display the excerpt */}
           <p className="text-lg text-gray-500">{post.excerpt}</p>
         </div>
-        
         <hr className="my-8 border-t border-gray-300" />
-
-        {/* Display the content */}
-        <div className="content-indent">
+        <div className="content">
           <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
         </div>
-        
-        {/* Display the author and date info */}
         <div className="text-left text-orange-600 py-3">
           <p>Published {post.datePublished}</p>
         </div>
